cd3b3a9e1e7a07b10fa224c123f26f1136c0e1c6
Pavel Pronevich <pavelpronevich@gmail.com>
2014-10-04 12:53:38 +0300

    fsfs
    dsfffffffffs

---
+++ zadacha 1/zadacha 1/Ivagetable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacha_1
+{
+    interface Ivagetable
+    {
+        int getColorii();
+        string getIso();
+        string getName();
+
+
+    }
+}
---
+++ zadacha 1/zadacha 1/Root vegetables.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacha_1
+{
+    class Root_vegetables: Ivagetable
+    {
+        public string vegetableType="Root vegetables";
+        public string vegetableName;
+        private string vegetableIso;
+        private int colorii;
+        public int getColorii()
+        {
+            return colorii;
+        }
+        public string getIso()
+        {
+            return vegetableIso;
+        }
+        public string getName()
+        {
+            return vegetableName;
+        }
+
+        
+
+
+    }
+}
