10398ec081c3bffa5d8914808146446ee3f74543
Pavel Pronevich <pavelpronevich@gmail.com>
2014-10-06 02:28:32 +0300

    06-10-2.28

--- zadacha 1/zadacha 1/Program.cs
+++ zadacha 1/zadacha 1/Program.cs
@@ -10,15 +10,22 @@ namespace zadacha
     {
         static void Main(string[] args)
         {
+            // нулевая масса 
             carrot  ingr1 = new carrot();
-            ingr1.massa = 200;
+            ingr1.massa = 0;
+                                
             
-            Console.WriteLine("В {0} граммов {1} содержится {2} грамма протеина", ingr1.massa,ingr1.getName(),ingr1.getProteins() );
+            //тестирование 
+            Console.WriteLine("{0}, {1}, {2}, б {3}, ж {4}, у{5}, к{6}, общ кал {7}", ingr1.getVegetableType(), ingr1.getName(), 
+                ingr1.getIso(), ingr1.getProteins(), ingr1.getFats(), ingr1.getCarbohydrates(), ingr1.getCalories(), ingr1.getEnergeticValue());
             Console.ReadKey();
-            Console.WriteLine(ingr1.getVegetableType());
+            radish ingr2 = new radish();
+            ingr2.massa = 200;
+            Console.WriteLine("{0}, {1}, {2}, б {3}, ж {4}, у{5}, к{6}, общ кал {7}", ingr2.getVegetableType(), ingr2.getName(),
+                ingr2.getIso(), ingr2.getProteins(), ingr2.getFats(), ingr2.getCarbohydrates(), ingr2.getCalories(), ingr1.getEnergeticValue());
+
             Console.ReadKey();
-            //
- 
+           
             
         }
     }
--- zadacha 1/zadacha 1/Tomato.cs
+++ zadacha 1/zadacha 1/Tomato.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace zadacha
 {
-    class RootVegetables : BaseClass   
+    class Tomato: BaseClass   
     {
-        private string vegetableType="Root vegetables (корнеплоды)";
+        private string vegetableType = "Tomato (томатные овощи)";
         public string getVegetableType() 
         {
             return vegetableType;
@@ -16,15 +16,15 @@ namespace zadacha
        
              
     }
-    class carrot:  RootVegetables
+    class tomatoes : Tomato
     {
-        private string vegetableName = "Carrot (морковь)";
-        private string vegetableIso = "3216771";
+        private string vegetableName = "tomatoes (помидоры)";
+        private string vegetableIso = "3325534771";
         public double massa=0;
         
-        private const double proteins = 1.2;
-        private const double fats = 0.1;
-        private const double carbohydrates = 7.2;
+        private const double proteins = 1.1;
+        private const double fats = 0.2;
+        private const double carbohydrates = 3.8;
         public string getName()
         {
             return vegetableName;
@@ -57,55 +57,15 @@ namespace zadacha
     }
 
 
-    class radish : RootVegetables
+    class bellPeppers : Tomato
     {
-        private string vegetableName = "radish (редис)";
-        private string vegetableIso = "32829371";
+        private string vegetableName = "bell peppers (болгарский перец)";
+        private string vegetableIso = "2048882201";
         public double massa = 0;
 
-        private const double proteins = 1.2;
-        private const double fats = 0.1;
-        private const double carbohydrates = 3.8;
-        public string getName()
-        {
-            return vegetableName;
-        }
-        public string getIso()
-        {
-            return vegetableIso;
-        }
-        public double getProteins()
-        {
-            return getProteins(proteins, massa);
-        }
-
-        public double getFats()
-        {
-            return getFats(fats, massa);
-        }
-        public double getCarbohydrates()
-        {
-            return getCarbohydrates(carbohydrates, massa);
-        }
-        public double getCalories()
-        {
-            return getCalories(proteins, fats, carbohydrates);
-        }
-        public double getEnergeticValue()
-        {
-            return getEnergeticValue(massa, proteins, fats, carbohydrates);
-        }
-    }
-
-    class beets : RootVegetables
-    {
-        private string vegetableName = "beets (свекла отварная)";
-        private string vegetableIso = "1839371";
-        public double massa = 0;
-
-        private const double proteins = 1.8;
-        private const double fats = 0;
-        private const double carbohydrates = 10.8;
+        private const double proteins = 1.3;
+        private const double fats = 0.0;
+        private const double carbohydrates = 5.3;
         public string getName()
         {
             return vegetableName;
