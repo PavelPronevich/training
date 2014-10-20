c8d22e03cd24b6b1077b00b0b62fdba136db69e0
PavelPronevich <pavelpronevich@gmail.com>
2014-10-21 02:03:44 +0300

    20-10

---
+++ Task 2 New/Task 2 New/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2_New
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String A = System.IO.File.ReadAllText(@"E:\epam\WriteText.txt"+" ");
+
+            //удаление форматирования
+
+            A = A.Replace("\n", " ");
+            A = A.Replace("\t", " ");
+            A = A.Replace("\r", " ");
+            
+            //удаление парных пробелов
+            string prob = "  ";
+            while (A.Contains(prob))
+            {
+                A = A.Remove(A.IndexOf(prob), 1);
+            }
+            //Console.WriteLine(A);
+            
+            // выделение предложений (повествовательных, вопросительных, побудительных)
+
+            
+            int i,j,k,number;
+            int index=0;
+            number = 0;
+            string e;
+
+            while (A.IndexOf(". ")!=A.LastIndexOf(". "))
+            {
+                if (A.Contains(". "))
+                {
+                    i = A.IndexOf(". ");
+                    if (A.Contains("! "))
+                    {
+                        j = A.IndexOf("! ");
+
+                        if (A.Contains("? "))
+                        {
+                            k = A.IndexOf("? ");
+                            if ((i < j) && (i < k))
+                            {
+                                Console.WriteLine(A.Substring(0,i));
+                                number = i + 2;
+                            }
+                            if ((j < i) && (j < k))
+                            {
+                                Console.WriteLine(A.Substring(0,j));
+                                number += j + 2;
+                            }
+                            if ((k < i) && (k < j))
+                            {
+                                Console.WriteLine(A.Substring(0,k));
+                                number = k + 2;
+                            }
+                        }
+                        else
+                        {
+                            if (i < j)
+                            {
+                                Console.WriteLine(A.Substring(0,i));
+                                number = i + 2;
+                            }
+                            else
+                            {
+                                Console.WriteLine(A.Substring(0,j));
+                                number = j + 2;
+                            }
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine(A.Substring(0,i));
+                        number = i + 2;
+                    }
+                }
+                A = A.Remove(0, number);
+                
+            }
+            Console.ReadKey();
+           
+            
+
+
+           
+        }
+    }
+}
--- Task 2 New/Task 2 New/Properties/AssemblyInfo.cs
+++ Task 2 New/Task 2 New/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Task2")]
+[assembly: AssemblyTitle("Task 2 New")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Task2")]
+[assembly: AssemblyProduct("Task 2 New")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8a47fa63-22d7-467d-bd28-665ffc3a2b0c")]
+[assembly: Guid("87b450fa-5364-41d2-a7b0-d14eac096201")]
 
 // Version information for an assembly consists of the following four values:
 //
